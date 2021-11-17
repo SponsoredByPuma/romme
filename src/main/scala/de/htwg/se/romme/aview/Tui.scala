660c28bb68eb0de612b626f4dc8d93be9658d4cc
SponsoredByPuma <tillwanner01@gmail.com>
2021-11-17 09:30:58 +0100

    test

--- src/main/scala/de/htwg/se/romme/aview/Tui.scala
+++ src/main/scala/de/htwg/se/romme/aview/Tui.scala
@@ -31,6 +31,7 @@ class Tui(controller: Controller) extends de.htwg.se.romme.util.Observer {
       case _ =>   
     }
   }
+  // test
 
   override def update: Unit = println() // showCards()
   override def updated: Boolean = true
