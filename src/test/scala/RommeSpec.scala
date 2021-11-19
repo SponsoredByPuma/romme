3e5a03bafacb9a3c0ddd7399361b66f5737c47b8
SponsoredByPuma <tillwanner01@gmail.com>
2021-11-19 12:22:01 +0100

    RommeSpec created

---
+++ src/test/scala/RommeSpec.scala
@@ -0,0 +1,12 @@
+package de.htwg.de.sudoku
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+
+class RommeSpec extends AnyWordSpec {
+  "The Romme main class" should {
+    "accept text input as argument without readline loop, to test it from command line " in {
+      de.htwg.se.romme.Romme.main(Array[String]("new"))
+    }
+  }
+}
